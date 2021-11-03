327cd41f488fa64224d5416348ec5f56d185479e
gyzzako <arnaud.berg1999@gmail.com>
2021-11-03 20:28:28 +0100

    active table css

--- src/components/backOffice/ActionBar.js
+++ src/components/backOffice/ActionBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './BackOffice.module.css';
+import css from './css.css';
 import {Navbar, Container} from 'react-bootstrap';
 
 
@@ -8,11 +9,17 @@ class ActionBar extends React.Component{
         super(props);
         this.state={
             categories:["Meal", "User", "Order", "Category"],
-            chosenTable: props.chosenTableCallback
+            chosenTable: props.chosenTableCallback,
+            clickedCategoryIndex: undefined
         }
     }    
 
+    setClicked = (number) => {
+        this.setState({clickedCategoryIndex: number });
+      };
+
     render(){
+        const isActive = this.state.isActive;
         return(
         <div className={classes.category}>
             <h2 style={{color: 'white'}}>Tables</h2>
@@ -21,9 +28,12 @@ class ActionBar extends React.Component{
             {
                 this.state.categories.map((category, index) => {
                     return(
-                        <Navbar key={index}>
+                        <Navbar>
                             <Container>
-                                <Navbar.Brand onClick={() => this.state.chosenTable(category.toLocaleLowerCase())} style={{color: 'white', cursor: 'pointer'}}>{category}</Navbar.Brand>
+                                <Navbar.Brand key={index} className={index === this.state.clickedCategoryIndex ? "clicked" : null} onClick={() => {
+                                    this.state.chosenTable(category.toLocaleLowerCase());
+                                    this.setClicked(index);
+                                }} style={{color: 'white', cursor: 'pointer'}}>{category}</Navbar.Brand>
                             </Container>
                         </Navbar>
                     )
